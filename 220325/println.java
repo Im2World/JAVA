4341a4a6d49888a153cd51c72718d83725467f70
Im2World <hi.programmer7@gmail.com>
2022-03-25 23:46:28 +0900

    datatype, if_else, println, switchCondition

---
+++ 220325/println.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class println {
+	enum Coffefranchise{
+		STARBUCKS, TWOSOMEPLACE, EDIYA};
+		
+	public static void main(String[] args) {
+		System.out.println(Coffefranchise.STARBUCKS);
+		System.out.println(Coffefranchise.TWOSOMEPLACE);
+		System.out.println(Coffefranchise.EDIYA);
+		}
+}
